3d1deefef16dbd481c586ca4fdcfa871a3823eaa
balvarezescudero <balvarezescudero@atanasoff30.danielcastelao.org>
2015-11-09 09:18:48 +0100

    Commit1

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -26,7 +26,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
-
+ //Commit1
 /* Anagram Game Application */
 
 package com.toy.anagrams.lib;
